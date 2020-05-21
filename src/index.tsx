5562517ac13d9d45078a23a2526b0aff17ba6b1b
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2020-05-21 22:38:40 +0200

    feat: :sparkles: add video

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'react-dom'
 import * as React from 'react'
-import { Suspense, useEffect, useRef } from 'react'
+import { Suspense, useEffect, useRef, useState } from 'react'
+import * as THREE from 'three'
 import { Canvas } from 'react-three-fiber'
 
 import { Text } from './Text'
@@ -26,7 +27,39 @@ let MouseRotatingGroup: React.FC<{}> = ({ children }) => {
   return <group ref={ref}>{children}</group>
 }
 
+let initWebcamInput = async () => {
+  let stream = await navigator.mediaDevices.getUserMedia({ video: true })
+  let video = document.createElement('video')
+
+  video.srcObject = stream
+  await video.play()
+
+  return video
+}
+
+let VideoMesh = ({ video }) => (
+  <mesh position={[0, -7, 0]}>
+    <boxBufferGeometry attach="geometry" args={[25, 15, 5]} />
+
+    <meshBasicMaterial attach="material">
+      <videoTexture
+        attach="map"
+        minFilter={THREE.LinearFilter}
+        magFilter={THREE.LinearFilter}
+        format={THREE.RGBFormat}
+        args={[video]}
+      />
+    </meshBasicMaterial>
+  </mesh>
+)
+
 let App = () => {
+  let [video, setVideo] = useState<HTMLVideoElement>()
+
+  useEffect(() => {
+    initWebcamInput().then(setVideo)
+  })
+
   return (
     <Canvas camera={{ position: [0, 0, 35] }}>
       <Suspense fallback={null}>
@@ -34,8 +67,10 @@ let App = () => {
         <pointLight position={[40, 40, 40]} />
 
         <MouseRotatingGroup>
-          <Text position={[0, 5, 0]}>ZUYS</Text>
-          <Text position={[0, 0, 0]}>BERLOGA</Text>
+          <Text position={[0, 10, 0]}>ZUYS</Text>
+          <Text position={[0, 5, 0]}>BERLOGA</Text>
+
+          {video && <VideoMesh video={video} />}
         </MouseRotatingGroup>
       </Suspense>
     </Canvas>
